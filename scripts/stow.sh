e22bfde5fccf39b6318b94ca69320481691403d3
Dmytro Somliev <dmytro.somliev@ibm.com>
2025-04-14 13:36:29 +0200

    Fix stow script

--- scripts/stow.sh
+++ scripts/stow.sh
@@ -6,11 +6,11 @@ echo "🎯 Stowing dotfiles into your system..."
 
 DOTFILES_DIR="$HOME/dotfiles"
 
-# Stow individual files
-stow --dir="$DOTFILES_DIR" --target="$HOME" --verbose .gitconfig
-stow --dir="$DOTFILES_DIR" --target="$HOME" --verbose .gitignore_global
-stow --dir="$DOTFILES_DIR" --target="$HOME" --verbose githelpers
-stow --dir="$DOTFILES_DIR" --target="$HOME" --verbose .ideavimrc
+# Symlink individual files
+ln -sf "$DOTFILES_DIR/.gitconfig" "$HOME/.gitconfig"
+ln -sf "$DOTFILES_DIR/.gitignore_global" "$HOME/.gitignore_global"
+ln -sf "$DOTFILES_DIR/.ideavimrc" "$HOME/.ideavimrc"
+ln -sf "$DOTFILES_DIR/githelpers" "$HOME/githelpers"
 
 # Stow .config subdirectories
 stow --dir="$DOTFILES_DIR/.config" --target="$HOME/.config" --verbose alacritty
